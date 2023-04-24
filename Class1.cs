fd4fe004afdc749150fc86108f1be516e5a7a485
whoami1925 <118896499+whoami1925@users.noreply.github.com>
2023-04-24 09:14:09 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace RoomLibraryTommin
+{
+    public class Room
+    {
+        double roomLength;
+        double roomWidth;
+
+        public double Roomlength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double RoomPerimeter()
+        {
+            return 2 * (roomLength + roomWidth);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="np"></param>
+        /// <returns></returns>
+        public double PersonaArea(int np)
+        {
+            return RoomArea() / np;
+        }
+    }
+}
