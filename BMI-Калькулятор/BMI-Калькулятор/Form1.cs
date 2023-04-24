f1513942b3081659215cbdd4b39efd8b57ae0e0f
whoami1925 <118896499+whoami1925@users.noreply.github.com>
2023-04-24 08:52:15 +0300

    Add files via upload

---
+++ BMI-Калькулятор/BMI-Калькулятор/Form1.cs
@@ -0,0 +1,94 @@
+﻿using Racist_simulator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Калькулятор
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            man.BorderStyle = BorderStyle.FixedSingle; ;
+            man.BackColor = Color.Gainsboro;
+        }
+        private void qqq(object sender, EventArgs e)
+        {
+            woman.BorderStyle = BorderStyle.None;
+            man.BorderStyle = BorderStyle.FixedSingle;
+            man.BackColor = Color.Gainsboro;
+            woman.BackColor = Color.WhiteSmoke;
+        }
+
+        private void qq(object sender, EventArgs e)
+        {
+            man.BorderStyle = BorderStyle.None;
+            woman.BorderStyle = BorderStyle.FixedSingle;
+            man.BackColor = Color.WhiteSmoke;
+            woman.BackColor = Color.Gainsboro;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Weights = Convert.ToDouble(weight.Text);
+            double Heights = Convert.ToDouble(height.Text);
+            Heights = Heights / 100;
+            double BMI = Math.Round(Weights / (Heights * Heights),1);
+            if (man.BackColor == Color.Gainsboro)
+            {
+                picture.Image = Properties.Resources.male_icon;
+            }
+            else
+                picture.Image = Properties.Resources.female_icon;
+            if (BMI < 18.5)
+            {
+                trackBar1.Value = 10;
+                BMItext.Text = "Недовес";
+
+            }
+            else if (BMI <= 24.9)
+            {
+                trackBar1.Value = 15;
+                BMItext.Text = "Здоровый";
+
+            }
+            else if (BMI <= 29.9)
+            {
+                trackBar1.Value = 25;
+                BMItext.Text = "Избыточный вес";
+
+            }
+            else if (BMI >= 30)
+            {
+                trackBar1.Value = 35;
+                BMItext.Text = "Ожирение";
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form Race = new Map();
+            Race.Show();
+
+        }
+    }
+}
---
+++ BMI-Калькулятор/BMI-Калькулятор/Map.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Racist_simulator
+{
+    public partial class Map : Form
+    {
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checkpoint 2";
+            label3.Text = "Новодевичей монастырь";
+            label5.Visible = true;
+            label6.Visible=true;
+            label7.Visible = true;
+            label8.Visible = true;
+            label9.Visible = true;
+            pictureBox14.Visible = true;
+            pictureBox16.Visible = true;
+            pictureBox17.Visible = true;
+            pictureBox18.Visible = true;
+            pictureBox15.Visible = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            button1.Visible = false;
+            pictureBox14.Visible = false;
+            pictureBox15.Visible = false;
+            pictureBox16.Visible = false;
+            pictureBox17.Visible = false;
+            pictureBox18.Visible = false;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checkpoint 1";
+            label3.Text = "МЦК Лужники";
+            label5.Visible = true;
+            pictureBox14.Visible = true;
+            label6.Visible = true;
+            pictureBox15.Visible = true;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checkpoint 3";
+            label3.Text = "Метро Киевская";
+            label5.Visible = false;
+            label5.Visible = true;
+            pictureBox14.Visible = true;
+            label6.Visible = true;
+            pictureBox15.Visible = true;
+            label7.Visible= true;
+            pictureBox16.Visible= true;  
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checkpoint 4";
+            label3.Text = "МИД";
+            label5.Visible = true;
+            pictureBox14.Visible = true;
+            label6.Visible = true;
+            pictureBox15.Visible = true;
+            label7.Visible = true;
+            pictureBox16.Visible = true;
+            label9.Visible = true;
+            pictureBox18.Visible = true;
+            //label9.Location = new Point(42; 211);
+            //pictureBox18.Location = new Point(10; 207);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checpoint 5";
+            label3.Text = "Парк Горького";
+            label5.Visible = true;
+            pictureBox14.Visible = true;
+            label6.Visible = true;
+            pictureBox15.Visible = true;
+            label7.Visible = true;
+            pictureBox16.Visible = true;
+            label8.Visible= true;
+            pictureBox17.Visible = true;
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checkpoint 6";
+            label3.Text = "Здание РАН";
+            label5.Visible = true;
+            pictureBox14.Visible = true;
+            label6.Visible = true;
+            pictureBox15.Visible = true;
+            label7.Visible = true;
+            pictureBox16.Visible = true;
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checkpoint 7";
+            label3.Text = "Метро Воробевы горы";
+            label5.Visible = true;
+            pictureBox14.Visible = true;
+            label6.Visible = true;
+            pictureBox15.Visible = true;
+            label7.Visible = true;
+            pictureBox16.Visible = true;
+            label8.Visible = true;
+            pictureBox17.Visible = true;
+            label9.Visible = true;
+            pictureBox18.Visible = true;
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label1.Text = "checkpoint 8";
+            label3.Text = "Стадион Лужники";
+            label5.Visible = true;
+            pictureBox14.Visible = true;
+            label6.Visible = true;
+            pictureBox15.Visible = true;
+            label7.Visible = true;
+            pictureBox16.Visible = true;
+            label8.Visible = true;
+            pictureBox17.Visible = true;
+            label9.Visible = true;
+            pictureBox18.Visible = true;
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender,e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "Начало гонки";
+            label3.Text = "Самба, полный марафон";
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "Начало гонки";
+            label3.Text = "Джонго, половина марафона";
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+            button1.Visible = true;
+            label1.Visible = true;
+            label3.Visible = true;
+            label1.Text = "Начало гонки";
+            label3.Text = "Капоэйра, весёлый забег на 5 километров";
+        }
+    }
+}
